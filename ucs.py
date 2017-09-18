f615df84527057d94da43aeb444b867d685dc2b7
EduardoVaca <vacalalo@gmail.com>
2017-09-18 12:45:49 -0500

    Add method to obtain all possible action from state

--- ucs.py
+++ ucs.py
@@ -20,6 +20,13 @@ class State:
         return state_str[1:]
 
     def result(self, action, height_limit):
+        """Makes an action on the current state.
+        PARAMS:
+        - action: tuple 0: start, 1: finish representing movement of container.
+        - height_limit: limit of the height of the stack.
+        RETURNS:
+        - True if the action was possible, False if not.
+        """
         if len(self.stack_containers[action[1]]) < height_limit and self.stack_containers[action[0]]:
             container = self.stack_containers[action[0]].pop()
             self.stack_containers[action[1]].append(container)
@@ -27,17 +34,34 @@ class State:
         else:
             return False
 
+    def possible_actions(self, visited_states, height_limit):
+        """Generates the possible actions from a current state.
+        PARAMS:
+        - visited_states: set representing visited states.
+        - height_limit: limit of the height of the stack.
+        RETURNS:
+        - List of tuples representing actions.
+        """
+        actions = []
+        for i in range(len(self.stack_containers)):
+            for j in range(i+1, len(self.stack_containers)):
+                if self.result((i, j), height_limit):
+                    if str(self) not in visited_states:
+                        actions.append((i, j))
+                    self.result((j, i), height_limit) # return to original state            
+        for i in range(len(self.stack_containers)):
+            for j in range(0, i):
+                if self.result((i, j), height_limit):
+                    if str(self) not in visited_states:
+                        actions.append((i, j))
+                    self.result((j, i), height_limit) # return to original state            
+        return actions
 
-state = State('(X, Y,  Z); (); (C)')
+
+state = State('(X, Y); (A, B); (C)')
 print(state)
-print(len(state.stack_containers[1]))
-state.result((0, 1), 3)
-print(state)
-print(len(state.stack_containers[1]))
-state.result((1, 2), 2)
-print(state)
-state.result((0, 2), 2)
-print(state)
+actions_l = state.possible_actions({}, 2)
+print(actions_l)
 
 
 
