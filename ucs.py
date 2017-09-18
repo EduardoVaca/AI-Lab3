1aa8ac381fbb5ea8bb47db3d8ed979dfc718a418
EduardoVaca <vacalalo@gmail.com>
2017-09-18 16:35:52 -0500

    Add better method

--- ucs.py
+++ ucs.py
@@ -19,23 +19,28 @@ class State:
             state_str += ';(' + ','.join(stack) + ')'
         return state_str[1:]
 
-    def result(self, action, height_limit):
-        """Makes an action on the current state.
+    def is_action_possible(self, action, height_limit):
+        """Checks if a following action is possible from current state.
         PARAMS:
         - action: tuple 0: start, 1: finish representing movement of container.
         - height_limit: limit of the height of the stack.
         RETURNS:
         - True if the action was possible, False if not.
         """
-        if len(self.stack_containers[action[1]]) < height_limit and self.stack_containers[action[0]]:
+        return len(self.stack_containers[action[1]]) < height_limit and self.stack_containers[action[0]]
+
+    def result(self, action, height_limit):
+        """Makes an action on the current state.
+        PARAMS:
+        - action: tuple 0: start, 1: finish representing movement of container.
+        - height_limit: limit of the height of the stack.
+        """
+        if self.is_action_possible(action, height_limit):
             container = self.stack_containers[action[0]].pop()
             self.stack_containers[action[1]].append(container)
-            return True
-        else:
-            return False
 
     def possible_actions(self, visited_states, height_limit):
-        """Generates the possible actions from a current state.
+        """Generates all the possible actions from a current state.
         PARAMS:
         - visited_states: set representing visited states.
         - height_limit: limit of the height of the stack.
@@ -45,22 +50,18 @@ class State:
         actions = []
         for i in range(len(self.stack_containers)):
             for j in range(i+1, len(self.stack_containers)):
-                if self.result((i, j), height_limit):
-                    if str(self) not in visited_states:
-                        actions.append((i, j))
-                    self.result((j, i), height_limit) # return to original state            
+                if self.is_action_possible((i, j), height_limit) and str(self) not in visited_states:
+                    actions.append((i, j))
         for i in range(len(self.stack_containers)):
             for j in range(0, i):
-                if self.result((i, j), height_limit):
-                    if str(self) not in visited_states:
-                        actions.append((i, j))
-                    self.result((j, i), height_limit) # return to original state            
+                if self.is_action_possible((i, j), height_limit) and str(self) not in visited_states:
+                    actions.append((i, j))
         return actions
 
 
 state = State('(X, Y); (A, B); (C)')
 print(state)
-actions_l = state.possible_actions({}, 2)
+actions_l = state.possible_actions({}, 3)
 print(actions_l)
 
 
