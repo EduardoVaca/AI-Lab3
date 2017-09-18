3b4c4ce91ec2379e34bca9bc00cf419ea983c0e3
EduardoVaca <vacalalo@gmail.com>
2017-09-18 10:03:04 -0500

    Add result state methid

--- ucs.py
+++ ucs.py
@@ -9,17 +9,35 @@ class State:
         for ch in [' ', '(', ')']:
             stacks_str = stacks_str.replace(ch, '')
         stacks = stacks_str.split(';')
-        self.container_stacks = []
+        self.stack_containers = []
         for stack in stacks:
-            self.container_stacks.append(stack.split(','))     
+            self.stack_containers.append([] if not stack else stack.split(','))     
 
     def __str__(self):
         state_str = ''
-        for stack in self.container_stacks:
+        for stack in self.stack_containers:
             state_str += ';(' + ','.join(stack) + ')'
         return state_str[1:]
 
-state = State('(x, y,  z); (); (C)')
+    def result(self, action, height_limit):
+        if len(self.stack_containers[action[1]]) < height_limit and self.stack_containers[action[0]]:
+            container = self.stack_containers[action[0]].pop()
+            self.stack_containers[action[1]].append(container)
+            return True
+        else:
+            return False
+
+
+state = State('(X, Y,  Z); (); (C)')
 print(state)
+print(len(state.stack_containers[1]))
+state.result((0, 1), 3)
+print(state)
+print(len(state.stack_containers[1]))
+state.result((1, 2), 2)
+print(state)
+state.result((0, 2), 2)
+print(state)
+
 
 
